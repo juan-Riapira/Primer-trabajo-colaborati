9b49152a4912e3f8df0386606c5150921d7108e0
BiancaAG289 <BiancaAG289>
2026-07-12 15:44:40 -0500

    feat: agregar funcion restar

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print("hola chicos")
 
 
+def restar(a, b):
+    return a - b
+
+# Ejemplo de uso
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+resultado = restar(num1, num2)
+
+print("El resultado de la resta es: ", {resultado})
