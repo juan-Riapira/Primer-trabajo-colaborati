b1c123c2cc88ab167a851db3c657af2f17924d72
aliss-mtt <alisonn.bri.om@gmail.com>
2026-07-12 12:35:48 -0500

    feat: crear funcion de dividir

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("hola chicos")
 
+print("Presiona (a) para dividir")
+opcion = input("Elige una opción:")
 
+if opcion =="a":
+    n1 = int(input("Primer numero:"))
+    n2 = int(input("Segundo numero:"))
+    
+    print("Resultado:", n1/n2)
