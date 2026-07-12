25286287621fedd1df5da6e33a59be44702938a5
samgames0228-svg <samgames0228@gmail.com>
2026-07-12 19:48:33 +0200

    2 commit: raizcuadrada

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import math
 print("hola chicos")
 
+x = int(input("Elige un numero:"))
+y = input("Elige lo que quieres hacer con el numero: (restar, sumar, multiplicar, dividir, raiz cuadrada)")
+
+if y == "raiz cuadrada":
+    if x >= 0:
+        root = math.sqrt(x)
+        print("La raiz cuadrada de", x, "es", root)
+    else:
+        print("Syntax Error")
 
